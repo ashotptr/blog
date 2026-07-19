bad2455d6331599965c8bae55fcffe2cba5e1984
AshotP <apetrosyan@wovenmedia.com>
2026-07-19 15:24:40 +0400

    change

--- Blog.Api/Dtos/LoginDto.cs
+++ Blog.Api/Dtos/LoginDto.cs
@@ -1,8 +1,9 @@
 namespace Blog.Api.Dtos;
 
 using System.ComponentModel.DataAnnotations;
-public class LoginDto 
-{ 
-    [Required] public string? Username { get; set; } 
-    [Required] public string? Password { get; set; } 
+
+public class LoginDto
+{
+    [Required] public string Username { get; set; } = string.Empty;
+    [Required] public string Password { get; set; } = string.Empty;
 }
--- Blog.Api/Dtos/RegisterDto.cs
+++ Blog.Api/Dtos/RegisterDto.cs
@@ -2,9 +2,9 @@ namespace Blog.Api.Dtos;
 
 using System.ComponentModel.DataAnnotations;
 
-public class RegisterDto 
-{ 
-    [Required] public string? Username { get; set; } 
-    [Required] [EmailAddress] public string? Email { get; set; } 
-    [Required] public string? Password { get; set; } 
+public class RegisterDto
+{
+    [Required] public string Username { get; set; } = string.Empty;
+    [Required][EmailAddress] public string Email { get; set; } = string.Empty;
+    [Required] public string Password { get; set; } = string.Empty;
 }
